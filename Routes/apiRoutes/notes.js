d588e7f8a2c3c0d6ff96d8a2866b6f3018464cf9
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-25 16:08:50 -0400

    corrected typos

--- Routes/apiRoutes/notes.js
+++ Routes/apiRoutes/notes.js
@@ -1,15 +1,15 @@
-const router = require("express").Router();
-const { createNewNote, getNotes, deleteNote } = require("../../lib/notes");
+const router = require('express').Router();
+const { createNewNote, getNotes, deleteNote } = require('../../lib/notes');
 // const { notes } = require("./public/notes");
-const allNotes = require("../../db/db.json");
+const allNotes = require('../../db/db.json');
 
-router.get("/notes", (req, res) => {
+router.get('/notes', (req, res) => {
     const allNotes = getNotes()
     return res.json(allNotes)
 });
 
  // push data to the db.json file
-router.get("/notes/:id", (req, res) => {
+router.get('/notes/:id', (req, res) => {
     newNote(req.params.id, allNotes);
     if (result) {
       res.json(result);
@@ -18,14 +18,14 @@ router.get("/notes/:id", (req, res) => {
     }
   });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
     const newNote = createNewNote(req.body);
     res.json(newNote);
 });
 
    
 
-router.delete("/notes/:id", (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     deleteNote(req.params.id, allNotes);
     res.json(true);
 });
